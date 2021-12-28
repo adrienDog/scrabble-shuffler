c14c165e67c9659964a9808c3069403a703a2fd0
adrienDog <adrien.vaschalde@gmail.com>
2021-12-28 11:52:46 +0100

    Add ability to save words

--- src/Container.jsx
+++ src/Container.jsx
@@ -1,37 +1,70 @@
 import { useState } from 'react';
 import SetOfLetters from './SetOfLetters';
 import UpdateLettersForm from './UpdateLettersForm';
+import SavedWords from './SavedWords';
 
 const Container = ({appName}) => {
   const [isEditMode, setIsEditMode] = useState(false);
-  const [letters, setLetters] = useState('abcdefg');
+  const [currentWord, setCurrentWord] = useState('abcdefg');
+  const [savedWords, setSavedWords] = useState(new Set());
+  const [letters, setLetters] = useState(currentWord);
+
+  const saveCurrentWord = () => {
+    const newSet = new Set([...savedWords, currentWord]);
+    setSavedWords(newSet);
+  }
+
+  const sortStr = (str) => {
+    return str.split('').sort().join('');
+  }
+
+  const saveNewLetters = () => {
+    if (sortStr(currentWord) !== sortStr(letters)) {
+      setCurrentWord(letters);
+      setSavedWords(new Set());
+    } else {
+      console.log('new letters are the same', letters);
+    }
+    toggleEditMode();
+  }
+
+  const toggleEditMode = () => {
+    setIsEditMode(!isEditMode)
+  }
 
   return (<>
     <header className="App-header">
       <div className="App-header-left">
-
+        <button style={{float: 'left'}} className="btn"
+          onClick={isEditMode ? saveNewLetters : toggleEditMode}>
+          { isEditMode ? 'save' : 'change letters'}
+        </button>
       </div>
       <div className="App-header-middle">
         {appName}
       </div>
       <div className="App-header-right">
-        <a href="#" style={{float: 'right'}} className="btn"
-          onClick={() => setIsEditMode(!isEditMode)}>
-          { isEditMode ? 'save' : 'edit'}
-        </a>
+        {!isEditMode && (<button style={{float: 'right'}} className="btn"
+          onClick={saveCurrentWord}>
+          save word
+        </button>)}
       </div>
       
     </header>
     <div style={{marginTop: '20px'}}>
       {!isEditMode &&
-        <SetOfLetters letters={letters} />}
+        <SetOfLetters 
+          letters={currentWord} 
+          onReordered={(newWord)=>setCurrentWord(newWord)}/>}
     
       {isEditMode && 
         <UpdateLettersForm 
-          text={letters} 
-          onChange={(newLetters) => setLetters(newLetters)}/
-        >}
+          text={currentWord} 
+          onChange={(newWord) => setLetters(newWord)}/>}
     </div>
+    {!isEditMode && (<div style={{marginTop: '20px'}}>
+      <SavedWords words={Array.from(savedWords)}/>
+    </div>)}
   </>);
 };
 
--- src/SetOfLetters.jsx
+++ src/SetOfLetters.jsx
@@ -8,32 +8,32 @@ const style = {
   width: '100%',
 };
 
-const SetOfLetters = ({letters}) => {
-    {
-      const arrayOfLetters = letters
-        .split('')
-        .map((text, index) => ({text, id: index + 1}));
-      const [cards, setCards] = useState(arrayOfLetters);
+const SetOfLetters = ({letters, onReordered}) => {
+  const arrayOfLetters = letters
+    .split('')
+    .map((text, index) => ({text, id: index + 1}));
+  const [cards, setCards] = useState(arrayOfLetters);
 
-      const moveCard = useCallback((dragIndex, hoverIndex) => {
-          const dragCard = cards[dragIndex];
-          setCards(update(cards, {
-              $splice: [
-                  [dragIndex, 1],
-                  [hoverIndex, 0, dragCard],
-              ],
-          }));
-      }, [cards]);
-      const renderCard = (card, index) => {
-          return (<Card key={card.id} index={index} id={card.id} text={card.text} moveCard={moveCard}/>);
-      };
-      
-      return (<>
-        <div style={style}>
-          {cards.map((card, i) => renderCard(card, i))}
-        </div>
-      </>);
-    }
+  const moveCard = useCallback((dragIndex, hoverIndex) => {
+      const dragCard = cards[dragIndex];
+      const updatedCards = update(cards, {
+        $splice: [
+            [dragIndex, 1],
+            [hoverIndex, 0, dragCard],
+        ],
+      });
+      setCards(updatedCards);
+      onReordered(updatedCards.map((c) => c.text).join(''));
+  }, [cards, onReordered]);
+  const renderCard = (card, index) => {
+      return (<Card key={card.id} index={index} id={card.id} text={card.text} moveCard={moveCard}/>);
+  };
+  
+  return (<>
+    <div style={style}>
+      {cards.map((card, i) => renderCard(card, i))}
+    </div>
+  </>);
 };
 
 export default SetOfLetters;
